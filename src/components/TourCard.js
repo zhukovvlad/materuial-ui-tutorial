80dc34fb7c873a3a0368ae914ba2ef53abee6e15
Zhukovvlad <zhukovvlad@yandex.ru>
2022-07-03 13:48:25 +0300

    Created Tour Card

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Grid, Paper, Typography, Box } from "@mui/material";
+import { Grid, Paper, Typography, Box, Rating } from "@mui/material";
+import { AccessTime } from "@mui/icons-material";
 
 const TourCard = () => {
   return (
@@ -20,8 +21,35 @@ const TourCard = () => {
               alignItems: "center",
             }}
           >
-            <Typography variant="body2" component="p">
+            <AccessTime sx={{ width: 12.5 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
               5 hours
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+                display: "flex",
+                alignItems: "center",
+            }}
+            marginTop={3}
+          >
+            <Rating
+                name="read-only"
+                value={4.5}
+                precision={0.5}
+                readOnly
+                size="small"
+            />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+                4.5
+            </Typography>
+            <Typography variant="body3" component="p" marginLeft={1.5}>
+                (655 reviews)
+            </Typography>
+          </Box>
+          <Box>
+          <Typography variant="h6" component="h3" marginTop={0}>
+                From C $147
             </Typography>
           </Box>
         </Box>
