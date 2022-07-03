8513d1e1a3f5e60a18fee2a983981e008f3dc92a
Zhukovvlad <zhukovvlad@yandex.ru>
2022-07-03 14:44:15 +0300

    Added AppBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import TourCard from "./components/TourCard";
+import SearchAppBar from "./components/AppBar";
 import { Container, Grid } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
+      <SearchAppBar />
       <Container>
         <Grid container spacing={5}>
           <TourCard />
