82b9556f48dd64b1abe6ff001fc6da3c7127d4d6
Zhukovvlad <zhukovvlad@yandex.ru>
2022-07-03 14:32:40 +0300

    Themed TourCard Component

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,59 +1,87 @@
 import React from "react";
 import { Grid, Paper, Typography, Box, Rating } from "@mui/material";
 import { AccessTime } from "@mui/icons-material";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          className="img"
-          src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
-          alt="tour"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Immerse into the Falls
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <AccessTime sx={{ width: 12.5 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            className="img"
+            src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
+            alt="tour"
+          />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              Immerse into the Falls
             </Typography>
-          </Box>
-          <Box
-            sx={{
+            <Box
+              sx={{
                 display: "flex",
                 alignItems: "center",
-            }}
-            marginTop={3}
-          >
-            <Rating
+              }}
+            >
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                5 hours
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+              marginTop={3}
+            >
+              <Rating
                 name="read-only"
                 value={4.5}
                 precision={0.5}
                 readOnly
                 size="small"
-            />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
                 4.5
-            </Typography>
-            <Typography variant="body3" component="p" marginLeft={1.5}>
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={1.5}>
                 (655 reviews)
-            </Typography>
-          </Box>
-          <Box>
-          <Typography variant="h6" component="h3" marginTop={0}>
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
                 From C $147
-            </Typography>
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
