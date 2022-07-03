ea00be0b2371dd66f4624515305baada94fac665
Zhukovvlad <zhukovvlad@yandex.ru>
2022-07-03 13:28:39 +0300

    Installed @mui/icons-material

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -10,10 +10,20 @@ const TourCard = () => {
           src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
           alt="tour"
         />
-        <Box padding={1}>
+        <Box paddingX={1}>
           <Typography variant="subtitle1" component="h2">
             Immerse into the Falls
           </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="body2" component="p">
+              5 hours
+            </Typography>
+          </Box>
         </Box>
       </Paper>
     </Grid>
