ce0871b246e403e96ddbd6eff6f082d46478c3e6
Zhukovvlad <zhukovvlad@yandex.ru>
2022-07-04 14:41:03 +0300

    Added props to TourCard

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -28,19 +28,15 @@ const theme = createTheme({
   },
 });
 
-const TourCard = () => {
+const TourCard = ({ tour }) => {
   return (
     <Grid item xs={3}>
       <ThemeProvider theme={theme}>
         <Paper elevation={3}>
-          <img
-            className="img"
-            src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
-            alt="tour"
-          />
+          <img className="img" src={tour.image} alt="tour" />
           <Box paddingX={1}>
             <Typography variant="subtitle1" component="h2">
-              Immerse into the Falls
+              {tour.name}
             </Typography>
             <Box
               sx={{
@@ -50,7 +46,7 @@ const TourCard = () => {
             >
               <AccessTime sx={{ width: 12.5 }} />
               <Typography variant="body2" component="p" marginLeft={0.5}>
-                5 hours
+                {tour.duration} hours
               </Typography>
             </Box>
             <Box
@@ -62,21 +58,21 @@ const TourCard = () => {
             >
               <Rating
                 name="read-only"
-                value={4.5}
+                value={tour.rating}
                 precision={0.5}
                 readOnly
                 size="small"
               />
               <Typography variant="body2" component="p" marginLeft={0.5}>
-                4.5
+                {tour.rating}
               </Typography>
               <Typography variant="body3" component="p" marginLeft={1.5}>
-                (655 reviews)
+                ({tour.numberOfReviews} reviews)
               </Typography>
             </Box>
             <Box>
               <Typography variant="h6" component="h3" marginTop={0}>
-                From C $147
+                From C ${tour.price}
               </Typography>
             </Box>
           </Box>
