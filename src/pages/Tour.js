9a6912bffd655767b6870c5018efd57fd7c422c3
Zhukovvlad <zhukovvlad@yandex.ru>
2022-07-04 16:43:06 +0300

    Finishe tutorial

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,5 +1,13 @@
-import { Container, Typography, Box } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Box,
+  Paper,
+  BottomNavigation,
+} from "@mui/material";
 import ImageCollage from "../components/ImageCollage";
+import Accordian from "../components/Accordian";
+import BasicModal from "../components/Modal";
 
 const Tour = () => (
   <Container sx={{ width: 900 }}>
@@ -15,10 +23,31 @@ const Tour = () => (
       <ImageCollage />
     </Box>
     <Box>
-    <Typography variant="h6" component="h4" marginTop={3}>
-      About this Ticket
-    </Typography>
+      <Typography variant="h6" component="h4" marginTop={3}>
+        About this Ticket
+      </Typography>
+      <Typography variant="paragraph" component="p" marginY={3}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit voluptates
+        rem quos delectus debitis earum modi, ipsum veritatis. Perferendis
+        officiis nobis, aut hic praesentium nulla vero, possimus omnis
+        reprehenderit blanditiis quis incidunt minima voluptatibus illum quam
+        corporis libero fugiat doloremque. Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Exercitationem officiis commodi beatae
+        animi incidunt necessitatibus aut provident ad ex. Saepe!
+      </Typography>
     </Box>
+    <Box marginBottom={10}>
+      <Typography variant="h6" component="h4" marginBottom={3}>
+        Frequently Asked Questions
+      </Typography>
+      <Accordian />
+    </Box>
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation showLabels><BasicModal /></BottomNavigation>
+    </Paper>
   </Container>
 );
 
